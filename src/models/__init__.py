4ab20f03e783a2d31743fd4839465854ae9bb577
longle325 <lebaolong74@gmail.com>
2026-01-06 12:33:34 +0700

    Remove Logistic Regression, SVM, and PhoBERT model implementations along with their configurations. Update model initialization to exclude these models from the available options in the Vietnamese ABSA framework.

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -3,28 +3,13 @@ Vietnamese ABSA Models
 
 Available models:
 - CRFModel: sklearn-crfsuite based CRF
-- LogRegModel: Logistic Regression with TF-IDF
-- SVMModel: SVM with TF-IDF
-- PhoBERTModel: PhoBERT + BiLSTM-CRF
 """
 
 from .base_model import BaseABSAModel
 from .crf_model import CRFModel
-from .logreg_model import LogRegModel
-from .svm_model import SVMModel
 
-# PhoBERT model requires extra dependencies
-try:
-    from .phobert_bilstm_crf import PhoBERTModel, PhoBERTBiLSTMCRF
-except ImportError:
-    PhoBERTModel = None
-    PhoBERTBiLSTMCRF = None
 
 __all__ = [
     'BaseABSAModel',
     'CRFModel',
-    'LogRegModel',
-    'SVMModel',
-    'PhoBERTModel',
-    'PhoBERTBiLSTMCRF'
 ]
